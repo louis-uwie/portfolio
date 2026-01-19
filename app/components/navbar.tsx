79f08e6674932e2d6985f3ee7da4f9299e74c1d8
uwie <louis.binwag@student.ateneo.edu>
2026-01-19 21:05:39 +0800

    Added sticky bar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavbarComponent() {
   return (
-    <nav className="">
+    <nav className="sticky top-0 z-50 bg-slate-950">
       <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-6">
         <Link href="/" className="text-xl font-bold hover:scale-110 transition-all duration-150">
           Home
