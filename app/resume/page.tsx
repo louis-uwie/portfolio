ea5b05bf3133da5c2ecdf0fc5f018982fd0a60ea
uwie <louis.binwag@student.ateneo.edu>
2026-01-19 18:40:00 +0800

    Included Resume Page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,10 +1,27 @@
-export default function ResumePage(){
+'use client'; // Ensure that server doesn't need to mind this component.
 
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/legacy/build/pdf.worker.min.mjs',
+    import.meta.url
+).toString();
+
+export default function PdfViewer() {
     return (
-        <div className="align-center justify-center items-center flex flex-col gap-4 p-8 mt-32">
-            <p className="text-xl font-bold">This page is under construction!</p>
-            Resume Page.
-        </div>
-    )
-
+    <main className="flex justify-center px-4">
+        <Document
+            file="/resume.pdf"
+            loading={null}
+            error={null}
+        >
+            <Page
+            pageNumber={1}
+            scale={1.5}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+            />
+        </Document>
+        </main>
+    );
 }
