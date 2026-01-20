17592cadba04acf2092642f55c83615c9058a62d
uwie <157711065+louis-uwie@users.noreply.github.com>
2026-01-20 09:15:09 +0800

    Minor fix

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -3,7 +3,7 @@ export default function ExperiencePage(){
     return (
         <div className="align-center justify-center items-center flex flex-col gap-4 p-8 mt-32">
             <p className="text-xl font-bold">This page is under construction!</p>
-            Education Page
+            Experience Page
         </div>
     )
 
