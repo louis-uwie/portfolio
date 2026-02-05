4b119c206961c0ba8513ca8cc4eb1a25b72c1e76
uwie <157711065+louis-uwie@users.noreply.github.com>
2026-02-05 13:00:34 +0800

    Update: Cleaning up

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -9,27 +9,25 @@ export default function EducationPage() {
       <p className="text-4xl font-bold">My Educational Background</p>
 
       <div className="education-list text-xl flex flex-col gap-4 border-t border-white ">
-
-
-        {education.map((edu) => ( 
+        {education.map((edu) => (
           <div
             key={edu.id}
             className="education-item p-4 flex items-center gap-8"
           >
             {/* Image */}
             <Image
-                src={`${edu.image}`}
-                alt={`${edu.institution} Logo`}
-                className="w-20 h-20 object-contain rounded-full"
-                width={300} height={300}
-              />
-            
+              src={`${edu.image}`}
+              alt={`${edu.institution} Logo`}
+              className="w-20 h-20 object-contain rounded-full"
+              width={300}
+              height={300}
+            />
 
             {/* Details */}
             <div className="education-details flex flex-col">
               <h2 className="text-2xl font-bold">{edu.degree}</h2>
               <a
-                href={edu.link} 
+                href={edu.link}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-white hover:underline"
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import { Outfit, Open_Sans } from 'next/font/google';
-import './globals.css';
+import { Outfit, Open_Sans } from "next/font/google";
+import "./globals.css";
 
 import NavbarComponent from "./components/navbar";
 
 const outfit = Outfit({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800', '900'],
-  style: ['normal'],
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal"],
 });
 
 const openSans = Open_Sans({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800'],
-  style: ['normal', 'italic'],
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  style: ["normal", "italic"],
 });
 
 export default function RootLayout({
@@ -25,10 +25,8 @@ export default function RootLayout({
       <body
         className={`antialiased ${outfit.className} ${openSans.className} `}
       >
-    
         <NavbarComponent />
         {children}
-
       </body>
     </html>
   );
