1ffc5012f1543743b73172bb3c30351f251c0169
uwie <157711065+louis-uwie@users.noreply.github.com>
2026-02-16 10:40:15 +0800

    Update: Fix image rendering

--- app/components/experienceCard.tsx
+++ app/components/experienceCard.tsx
@@ -1,66 +1,57 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
-
-export interface ExperienceCardProps {
-  id: number;
-  title: string;
-  company: string;
-  website: string;
-  year: number;
-  type: string;
-  image_dir: string;
-  description?: string;
-  tags?: string[];
-}
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
 
 export default function ExperienceCard({
   title,
-  image_dir,
+  image,
   description,
   tags,
   website,
 }: ExperienceCardProps) {
   return (
-    <Card style={{ maxWidth: 350, minHeight: 250, height: '100%' }}>
+    <Card sx={{ maxWidth: 350, minHeight: 250, height: "100%" }}>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={`${image_dir}`}
-          alt={title}
-        />
+        <CardMedia component="img" height="140" image={image} alt={title} />
+
         <CardContent>
-          <Typography gutterBottom variant="button">
-            {title} <a href={website} target="_blank" rel="noopener noreferrer">🔗</a>
+          <Typography gutterBottom variant="button" component="div">
+            {title}{" "}
+            <a href={website} target="_blank" rel="noopener noreferrer">
+              🔗
+            </a>
           </Typography>
 
           {description && (
             <Typography
-            variant="subtitle1"
-            sx={{
-                color: 'text.secondary',
-                display: '-webkit-box',
-                WebkitBoxOrient: 'vertical',
-                WebkitLineClamp: 4, 
-                overflow: 'hidden',
-            }}
+              variant="subtitle1"
+              sx={{
+                color: "text.secondary",
+                display: "-webkit-box",
+                WebkitBoxOrient: "vertical",
+                WebkitLineClamp: 4,
+                overflow: "hidden",
+              }}
             >
-            {description}
+              {description}
             </Typography>
-
           )}
 
           {tags && (
             <Typography
               variant="subtitle2"
-              sx={{ color: 'text.secondary', marginTop: '8px' , border: '1px solid #e0e0e0', padding: '4px', borderRadius: '4px'}}
+              sx={{
+                color: "text.secondary",
+                mt: 1,
+                border: "1px solid #e0e0e0",
+                p: 0.5,
+                borderRadius: 1,
+              }}
             >
-              {tags.map(tag => (
-                <span key={tag} style={{ marginRight: '4px' }}>
+              {tags.map((tag) => (
+                <span key={tag} style={{ marginRight: 4 }}>
                   #{tag}
                 </span>
               ))}
--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -4,16 +4,10 @@ import experienceData from "../data/experience.json";
 export default function ExperiencePage() {
   return (
     <div className="align-center justify-center items-center flex flex-col gap-12 p-8 mt-32">
-      <p className="text-xl font-bold">This page is under construction!</p>
-      Experience Page
-
       <div className="flex flex-col justify-center gap-12">
         <div className="flex flex-wrap justify-center gap-4">
           {experienceData.map((experience) => (
-            <ExperienceCard
-              key={experience.id}
-              {...experience}
-            />
+            <ExperienceCard key={experience.id} {...experience} />
           ))}
         </div>
       </div>
