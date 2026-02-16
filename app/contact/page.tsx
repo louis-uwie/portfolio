9bd258de312e8398c12a382ad200dc479c1cdefb
uwie <157711065+louis-uwie@users.noreply.github.com>
2026-02-16 12:07:35 +0800

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,10 @@
-export default function ContactPage(){
-
-    return (
-        <div className="align-center justify-center items-center flex flex-col gap-4 p-8 mt-32">
-            <p className="text-xl font-bold">This page is under construction!</p>
-            Contact Page.
-
-            <div className="flex flex-wrap justify-center items-center gap-2 mt-4">
-                <div className="w-70 h-30 bg-gray-500 animate-pulse">Email</div>
-                <div className="w-70 h-30 bg-gray-500 animate-pulse">LinkedIn</div>
-            </div>
-        </div>
-    )
-
+export default function ContactPage() {
+  return (
+    <div className="align-center justify-center items-center flex flex-col gap-4 p-8 mt-32">
+      <div className="flex flex-wrap justify-center items-center gap-2 mt-4">
+        <div className="w-70 h-30 bg-gray-500 animate-pulse">Email</div>
+        <div className="w-70 h-30 bg-gray-500 animate-pulse">LinkedIn</div>
+      </div>
+    </div>
+  );
 }
