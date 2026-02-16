431c5cca9ee871206a98a94ba329e22b1552a976
uwie <157711065+louis-uwie@users.noreply.github.com>
2026-02-16 16:11:19 +0800

    Update page.tsx

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -3,13 +3,13 @@ import experienceData from "../data/experience.json";
 
 export default function ExperiencePage() {
   return (
-    <div className="align-center justify-center items-center flex flex-col p-8 mt-12">
-      <div className="flex flex-col justify-center gap-12">
+    <div className="align-center justify-center items-center flex flex-col p-4 mt-12">
+      <div className="flex flex-col justify-center gap-4">
         <div className="flex flex-col justify-center items-center gap-4 mb-8 border-b border-white pb-4">
           <p className="text-4xl font-bold">Experiences</p>
         </div>
 
-        <div className="flex flex-wrap justify-center gap-16">
+        <div className="flex flex-wrap justify-center gap-8">
           {experienceData.map((experience) => (
             <ExperienceCard key={experience.id} {...experience} />
           ))}
